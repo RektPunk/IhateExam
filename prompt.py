28ec22724da2ba167d3cf9853f67cd2126418dec
RektPunk <rektpunk@gmail.com>
2025-05-05 01:00:24 +0900

    update prompt

--- prompt.py
+++ prompt.py
@@ -11,6 +11,6 @@ Instructions:
 - You must answer in Korean.
 - You must answer in a concise manner.
 - Carefully analyze the context and identify relevant answer that are directly related to the user question.
-- Before you answer the question, think twice for the answer is correct.
-- Respond with clear answer with explanations why you think that way.s
+- Before you answer the question, think twice or three times for the answer is correct.
+- Please respond with a clear answer and explanations of why you think those solutions work, based on the context provided.
 """
